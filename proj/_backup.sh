0264cf581a25d8137d65d4870648fda081e7450e
Palash A <pa334@cornell.edu>
2020-03-05 14:24:28 -0500

    proj init

---
+++ proj/_backup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+####  CS2043 Final Backup Project  ####
+
+# This file is a backup submission, in 
+# case the main project is not finished
+# in time. For details regarding that 
+# project, please see `README`
+
+# This program performs some prime ops
+# on a number entered. Ther user may
+# - check if a number is prime
+# - find the closest prime
+# - find a numbers prime factors
+# - enter a number and be surprised!
+
+####     see README for more       ####
+
+
+
+
+
