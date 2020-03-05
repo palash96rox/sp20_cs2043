c906328440a40ea7a80b45b1ba8a6056dc5f1a2e
Palash A <pa334@cornell.edu>
2020-03-05 16:02:03 -0500

    prime sturcture; some error

--- proj/prime.sh
+++ proj/prime.sh
@@ -13,10 +13,27 @@
 # - find the closest prime
 # - find a numbers prime factors
 # - enter a number and be surprised!
+# By default the program checks if the number is prime or not
 
 ####     see README for more       ####
 
+function isprime {
+  # [ `expr $1 % 2` -eq 0 ] && return 0
+  n=`bc <<< "scale=0; sqrt(($1))+1"`; i=3
+  while [ $i -le $n ]; do c=`expr $1 % $i`
+    echo "$i >> $c"
+    [ ! $c > 0 ] && break
+    i=`expr $i + 1`
+    echo " now $i "
+  done
+  # echo
+  echo $i
+  [ $i -eq $n ] && true || false
+}
 
+[ $# -eq 1 ] && if isprime $1
+  then echo "prime"
+  else echo "not"
+fi
 
-
-
+isprime $1 | echo
