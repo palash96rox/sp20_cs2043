cdced681e47433cf394d2dfddcb7839d2cdd3c2f
Palash A <pa334@cornell.edu>
2020-03-05 19:42:16 -0500

    opt 1 and 2 done

--- proj/prime.sh
+++ proj/prime.sh
@@ -12,7 +12,6 @@
 # - check if a number is prime
 # - find the closest prime
 # - find a numbers prime factors
-# - enter a number and be surprised!
 # By default the program checks if the number is prime or not
 
 ####     see README for more       ####
@@ -26,7 +25,36 @@ function isprime {
   [ $i -eq $(($n+1)) ] && true || false
 }
 
-[ $# -eq 1 ] && if isprime $1
-  then echo "prime"
-  else echo "not"
+function printmenu { clear
+  echo "-------  MENU  -------"
+  echo "1. check if prime"
+  echo "2. find closest prime"
+  echo "3. find prime factors"
+  echo -n "What you choose? "
+}
+
+if [ $# -eq 1 ]; then
+  if isprime $1; then echo "prime"
+    else echo "not"; fi
+  else printmenu
+    read n; case $n in
+      "1") echo -n "num? " && read x
+        if isprime $x; then echo "prime"
+          else echo "not"; fi
+        ;;
+      "2") echo -n "num? "; read x
+        if isprime $x; then echo "$x is already prime."
+          else a=$(($x-1)) && while ! isprime $a; do
+              a=$(($a-1)); done # lower prime
+            b=$(($x+1)) && while ! isprime $b; do
+              b=$(($b+1)); done # higher prime
+            if [ $(($x-$a)) -eq $(($b-$x)) ]; then echo \
+              "Special case: $x is the mean of $a and $b"
+              else [ $(($x-$a)) -le $(($b-$x)) ] && \
+                echo "-: $a" || echo "+: $b"; fi
+        fi;;
+      "3") echo -n "TODO"
+        ;;
+      *) echo "invalid option. please restart." && exit;;
+    esac
 fi
